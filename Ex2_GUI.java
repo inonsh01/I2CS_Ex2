31ba30fb9bacf13b13310b7b70de7da1b0451224
inonsh01 <inonsh01@gmail.com>
2025-12-21 18:03:00 +0200

    Gui: all functions + stdDraw

--- Ex2_GUI.java
+++ Ex2_GUI.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.awt.Color;
+
 /**
  * Intro2CS_2026A This class represents a Graphical User Interface (GUI) for
  * Map2D. The class has save and load functions, and a GUI draw function. You
@@ -8,7 +14,39 @@
  */
 public class Ex2_GUI {
     public static void drawMap(Map2D map) {
-        //
+        // edge case
+        if (map == null) {
+            System.err.println("Error: Cannot draw a null map.");
+            return;
+        }
+
+        // get dimensions
+        int width = map.getWidth();
+        int height = map.getHeight();
+
+        StdDraw.enableDoubleBuffering();
+
+        // set scales
+        StdDraw.setXscale(-0.5, width - 0.5);
+        StdDraw.setYscale(-0.5, height - 0.5);
+
+        // loop over the map
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+
+                // get the color
+                int color = map.getPixel(i, j);
+
+                // casting to Color object and set the pan color
+                Color c = new Color(color);
+                StdDraw.setPenColor(c);
+
+                // draw a 1x1 square at (i, j)
+                StdDraw.filledSquare(i, j, 0.5);
+            }
+        }
+        // display the map
+        StdDraw.show();
     }
 
     /**
@@ -17,7 +55,36 @@ public class Ex2_GUI {
      */
     public static Map2D loadMap(String mapFileName) {
         Map2D ans = null;
+        try {
 
+            // scan mapFile
+            Scanner scanner = new Scanner(new File(mapFileName));
+
+            // get dimensions
+            int width = scanner.nextInt();
+            int height = scanner.nextInt();
+
+            // create a new Map object
+            ans = new Map(new int[width][height]);
+
+            // loop all over the map like the file wrote (from the height to width)
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+
+                    // set the current pixel
+                    if (scanner.hasNextInt()) {
+                        ans.setPixel(i, j, scanner.nextInt());
+                    }
+                }
+            }
+            scanner.close();
+            System.out.println("File: " + mapFileName + " loaded successfully.");
+
+        }
+        catch (FileNotFoundException e) {
+            // if file not found present a error message
+            System.out.println("Error: " + e.getMessage());
+        }
         return ans;
     }
 
@@ -27,7 +94,46 @@ public class Ex2_GUI {
      * @param mapFileName
      */
     public static void saveMap(Map2D map, String mapFileName) {
+        if (map == null)
+            return;
 
+        try {
+            // create new file and define writer
+            File file = new File(mapFileName);
+            PrintWriter writer = new PrintWriter(file);
+
+            // get dimensions
+            int width = map.getWidth();
+            int height = map.getHeight();
+
+            // write dimensinons on top of file ( sort of convention )
+            writer.println(width + " " + height);
+
+            // loop over map to write it in file
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    // write the current pixel in file
+                    writer.print(map.getPixel(i, j));
+
+                    // space between numbers
+                    if (i < width - 1) {
+                        writer.print(" ");
+                    }
+                }
+                // \n
+                writer.println();
+            }
+
+            // close file
+            writer.close();
+
+            System.out.println("File: " + mapFileName + " saved successfully.");
+
+        }
+        catch (FileNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] a) {
